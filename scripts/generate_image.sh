4de48f94c0f3a88c5b7d5fe34ab9bdb8dc15f810
Lukas Gruber <grbr.lks@googlemail.com>
2023-12-06 07:26:37 +0100

    Add text

--- scripts/generate_image.sh
+++ scripts/generate_image.sh
@@ -31,7 +31,7 @@ URL=$(echo "$RESPONSE" | jq .data[0].url -r)
 REVISED_PROMPT=$(echo "$RESPONSE" | jq .data[0].revised_prompt -r)
 
 curl "$URL" -o "src/assets/$(date "+%s").png"
-echo "$REVISED_PROMPT" > "src/assets/$(date "+%s").txt"
+echo "{\"text\": \"$REVISED_PROMPT\"}" > "src/assets/$(date "+%s").json"
 
 # Check if curl was successful
 if [ $? -eq 0 ]; then
