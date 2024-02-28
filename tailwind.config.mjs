f443eca2e862d05776295c45665cf52fbe18ddec
Lukas Gruber <grbr.lks@googlemail.com>
2024-02-29 00:27:48 +0100

    add animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,27 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				blob: "blob 7s infinite ease-in-out"
+			},
+			keyframes: {
+				blob: {
+					"0%": {
+						transform: "translate(0px, 0px) scale(1)"
+					},
+					"33%": {
+						transform: "translate(-20px, 10px) scale(1.1)"
+					},
+					"66%": {
+						transform: "translate(30px, -30px) scale(0.9)"
+					},
+					"100%": {
+						transform: "translate(0px, 0px) scale(1)"
+					}
+				}
+			}
+		},
 	},
 	plugins: [],
 }
